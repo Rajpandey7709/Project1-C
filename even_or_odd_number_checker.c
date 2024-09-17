9d187b8267c1a2dfebecfe1513b8126069a81dfe
Rajpandey7709 <rajdhanishpandey@gmail.com>
2024-09-17 14:16:49 +0530

    even_or_odd_number_checker.c

--- even_or_odd_number_checker.c
+++ even_or_odd_number_checker.c
@@ -3,11 +3,10 @@
 int main() {
     int number;
 
-    // Ask the user for input
     printf("Enter an integer: ");
     scanf("%d", &number);
 
-    // Check if the number is even or odd
+   
     if (number % 2 == 0)
         printf("%d is even.\n", number);
     else
